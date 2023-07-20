1bebe7a7ffcf16e8d10c594fc4c6be4237ed427e
Gh0sty <reda.dohnane@gmail.com>
2023-07-19 19:28:16 -0700

    UPDATED OMG

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,19 +14,19 @@
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar(48); /* ASCII value for '0' */
-        return (0);
-    }
-    else if (n < 0)
-    {
-        _putchar('-');
-    }
-    return (-1);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48); /* ASCII value for '0' */
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
 }
